a32860951cb0ee3ac338211ab1ea8af96f91ac96
Anthy Chen <koyun.chen@gmail.com>
2023-05-04 14:19:19 -0700

    Added more validation and score

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
@@ -23,7 +24,11 @@ struct ContentView: View {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
                 }
-                Section {
+//                Section("Your score") {
+//                    Text("\(score)")
+//                        .font(.headline)
+//                }
+                Section(usedWords.isEmpty ? "" : "Used words") {
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
@@ -42,14 +47,35 @@ struct ContentView: View {
             } message: {
                 Text(errorMessage)
             }
+            .toolbar {
+                Button("New Word") {
+                    startGame()
+                }
+
+            }
+            .safeAreaInset(edge: .bottom) {
+                Text("\(score)")
+                    .font(.title)
+                    .frame(maxWidth: .infinity)
+                    .padding(.top, 30.0)
+                    .background(.blue)
+                    .foregroundColor(.white)
+            }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard !answer.isEmpty else { return }
         
         // word validation
+        guard answer.count >= 3 else {
+            wordError(title: "Word too short", message: "Word must be at least 3 or more letters!")
+            return
+        }
+        guard answer != rootWord else {
+            wordError(title: "Nice try...", message: "You can't use the root word as an answer!")
+            return
+        }
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Try something else!")
             return
@@ -63,13 +89,22 @@ struct ContentView: View {
             return
         }
         
+        calculateScoreFor(word: answer)
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
     }
     
+    func calculateScoreFor(word: String) {
+        score += (10 - (rootWord.count - word.count))
+    }
+    
     func startGame() {
+        newWord = ""
+        usedWords = []
+        score = 0
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
